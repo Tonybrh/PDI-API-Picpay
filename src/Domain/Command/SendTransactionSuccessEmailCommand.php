75caaade454070b934265b11b748ece2c36b4b19
Antônio Dias <antonio.eduardo@voxtec.com.br>
2024-10-28 17:12:22 -0300

    [FEAT] - Adicionando o comando para enviar email

---
+++ src/Domain/Command/SendTransactionSuccessEmailCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Domain\Command;
+
+use App\Domain\Handler\SendTransactionSuccessEmailHandler;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+
+#[AsCommand(
+    name: 'app:transaction:success',
+    description: 'Send email to the user confirming the transaction',
+)]
+class SendTransactionSuccessEmailCommand extends Command
+{
+    public function __construct(
+        private readonly SendTransactionSuccessEmailHandler $handler
+    ) {
+        parent::__construct();
+    }
+
+    #[\Override]
+    protected function configure(): void
+    {
+        $this
+            ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Dry run')
+        ;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    #[\Override]
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $this->handler->__invoke();
+
+        $io->success('All emails was sent!');
+
+        return Command::SUCCESS;
+    }
+}
--- src/Domain/Handler/SendTransactionSuccessEmailHandler.php
+++ src/Domain/Handler/SendTransactionSuccessEmailHandler.php
@@ -2,30 +2,36 @@
 
 namespace App\Domain\Handler;
 
+use App\Domain\Message\SendTransactionSuccessEmail;
+use Exception;
 use Symfony\Component\Mailer\Exception\TransportException;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Mime\Email;
 
 #[AsMessageHandler]
-class SendTransactionSuccessEmailHandler
+readonly class SendTransactionSuccessEmailHandler
 {
     public function __construct(
-        private readonly MailerInterface $mailer
+        private MailerInterface $mailer
     ) {
     }
 
-    public function __invoke($message)
+    /**
+     * @throws Exception|TransportExceptionInterface
+     */
+    public function __invoke(SendTransactionSuccessEmail $message): void
     {
         $email = (new Email())
             ->from('pdi@example.com')
             ->to("antoniodias1106@gmail.com")
-            ->text("Lindo demais");
+            ->text("Transação realizada com sucesso!!");
 
         try {
             $this->mailer->send($email);
         } catch (TransportException $e) {
-            // do something
+            throw new Exception("Erro ao enviar email");
         }
     }
 }
